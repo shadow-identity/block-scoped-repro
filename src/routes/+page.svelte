87287a71a7679f54e8c76937f4f48931b5c21a63
Pavel Nedrigailov <pavel.nedrigailov@invenium.io>
2025-05-16 11:08:52 +0200

    Actual reproduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+{#key "wrld"}
+  {@const name = "World"}
+  <p>Hello {name}</p>
+{/key}
+
+{#key "pvlk"}
+  {@const name = "Pavlik"}
+  <p>Hello {name}</p>
+{/key}
